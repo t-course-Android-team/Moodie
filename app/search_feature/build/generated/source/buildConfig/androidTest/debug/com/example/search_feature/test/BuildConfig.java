4d49428c2c5b89b358fc1a5ec10748218905a09e
el1dor <is927861@gmail.com>
2025-05-27 17:24:22 +0300

    search fragment

---
+++ app/search_feature/build/generated/source/buildConfig/androidTest/debug/com/example/search_feature/test/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.search_feature.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.search_feature.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String OPENROUTER_API_KEY = "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";
+}
---
+++ feature-search/src/main/java/com/example/feature_search/domain/SearchMovieUseCase.kt
@@ -0,0 +1,53 @@
+package com.example.feature_search.domain
+
+import com.example.feature_search.data.remote.ChatRequest
+import com.example.feature_search.data.remote.Message
+
+class SearchMovieUseCase(private val api: RemoteOpenRouterRepository , /*private val repository: WatchedMoviesRepository*/) {
+
+    suspend operator fun invoke(movie :MovieForSearch) : String{
+        return try{
+            with(movie) {
+                val prompt = buildString {
+                    append("pick 10 movies ")
+                    if (genre != null) append("in the genre $genre ")
+                    if (released != null) append("of these years $released ")
+                    if (mood!=null) append("in these move $mood ")
+                    if (country != null) append("from these country $country ")
+                    if (reference != null) append("similar to $reference ")
+                    append("Here is a list of movies that I have already watched. They don't need to be offered ")
+                    //                    repository.getWatchedMovies()?.map { movie -> movie.name }
+                    append("in the answer, give only the names of the films and nothing else")
+                    append("Format response EXACTLY like this: '1. Movie 1|2. Movie 2|...|10. Movie 10'")
+                }
+
+                val response = api.getChatResponse(
+                    ChatRequest(
+                        model ="deepseek/deepseek-chat-v3-0324:free",
+                        messages = listOf(
+                            Message(role= "user", content = prompt )
+                        )
+                    )
+                )
+                val movies = (response.body()?.choices?.firstOrNull()?.message?.content.toString())
+                return formatMoviesResponse(movies)
+            }
+
+
+        } catch (e: Exception){
+         return   "Error: ${e.message}"
+        }
+
+    }
+    private fun formatMoviesResponse(rawResponse: String): String {
+        return rawResponse
+            .replace("*", "")
+            .split('|')
+            .mapNotNull { line ->
+                val regex = """^\s*\d+[.)]\s*(.+?)\s*$""".toRegex()
+                regex.find(line.trim())?.groupValues?.get(1)
+            }
+            .mapIndexed { index, title -> "${index + 1}. $title" }
+            .joinToString("\t")
+    }
+}
